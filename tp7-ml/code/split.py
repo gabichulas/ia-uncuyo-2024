06f08d585d2a4a1a0d5191bc8efaa166c4ea7dbc
gabichulas <gabylopezromero13@gmail.com>
2024-10-16 20:51:53 -0300

    Mas avances B)

--- tp7-ml/code/split.py
+++ tp7-ml/code/split.py
@@ -2,3 +2,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('tp7-ml/data/arbolado-mza-dataset.csv')
+
+train_df, validation_df = train_test_split(df, test_size=0.2, random_state=2606)
+
+validation_df.to_csv('tp7-ml/data/arbolado-mendoza-dataset-validation.csv', index=False)
+train_df.to_csv('tp7-ml/data/arbolado-mendoza-dataset-train.csv', index=False)
