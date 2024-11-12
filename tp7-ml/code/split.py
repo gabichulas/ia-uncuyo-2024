8df2fde46e0af54c27e04429cd4d33958a5ba159
gabichulas <gabylopezromero13@gmail.com>
2024-11-12 01:43:36 -0300

    Done other things

--- tp7-ml/code/split.py
+++ tp7-ml/code/split.py
@@ -5,7 +5,5 @@ df = pd.read_csv('tp7-ml/data/arbolado-mza-dataset.csv')
 
 train_df, validation_df = train_test_split(df, test_size=0.2, random_state=2606)
 
-test_df, _ = train_test_split(df, test_size=0.2, random_state=1311)
 validation_df.to_csv('tp7-ml/data/arbolado-mendoza-dataset-validation.csv', index=False)
 train_df.to_csv('tp7-ml/data/arbolado-mendoza-dataset-train.csv', index=False)
-test_df.to_csv('tp7-ml/data/arbolado-mendoza-dataset-test.csv', index=False)
